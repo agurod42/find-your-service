49e7b0b4add075b05825bdd07e355a2dfc69067e
Agustín Rodríguez <me@agurodriguez.net>
2019-01-11 04:53:10 -0500

    CreateDatabase command

---
+++ api/app/Console/Commands/CreateDatabase.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class CreateDatabase extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'db:create {name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new database with the $_ENV[\'APP_NAME\'] name';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        DB::statement('CREATE DATABASE '.$this->argument('name'));
+    }
+}
