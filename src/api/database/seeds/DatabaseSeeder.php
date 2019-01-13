156071eb576c19a0ce5fbbb4e1f47509e614c68f
Agustín Rodríguez <me@agurodriguez.net>
2019-01-13 02:14:30 -0500

    (api): fix seeders

--- src/api/database/seeds/DatabaseSeeder.php
+++ src/api/database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            ServicesTableSeeder::class,
             UsersTableSeeder::class
         ]);
     }
--- src/api/database/seeds/ServicesTableSeeder.php
+++ src/api/database/seeds/ServicesTableSeeder.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class UsersTableSeeder extends Seeder
+class ServicesTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
